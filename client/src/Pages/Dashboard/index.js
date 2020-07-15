c6cda032863713eec8f3d48e5115be772a3235ba
robertyulisman <desainku1092@gmail.com>
2020-07-15 12:34:33 +0700

    update dashboard

--- client/src/Pages/Dashboard/index.js
+++ client/src/Pages/Dashboard/index.js
@@ -7,12 +7,16 @@ export class Dashboard extends Component {
     user: [],
     produk: [],
     kategori: [],
+    subkategori: [],
+    sewaitem: [],
   };
 
   componentDidMount() {
     this.getData();
     this.getProduk();
     this.getKategori();
+    this.getSubKategori();
+    this.getSewaItem();
   }
 
   getData = () => {
@@ -56,8 +60,36 @@ export class Dashboard extends Component {
         console.log(err);
       });
   };
+
+  getSubKategori = () => {
+    axios
+      .get('/api/subkategori')
+      .then((res) => {
+        // console.log('kategori data', res.data);
+        this.setState({
+          subkategori: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  getSewaItem = () => {
+    axios
+      .get('/api/sewaitem')
+      .then((res) => {
+        // console.log('sewaitem data', res.data);
+        this.setState({
+          sewaitem: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   render() {
-    const { user, produk, kategori } = this.state;
+    const { user, produk, kategori, subkategori, sewaitem } = this.state;
     return (
       <div>
         <Row type="flex">
@@ -168,6 +200,58 @@ export class Dashboard extends Component {
                 borderRadius: 10,
                 borderColor: 'grey',
                 margin: '5px 15px',
+                background: '#21bb03',
+                borderWidth: 1,
+                padding: 20,
+              }}
+            >
+              <Col span={6}>
+                <Icon
+                  type="shopping-cart"
+                  style={{
+                    fontSize: '60px',
+                    color: 'white',
+                  }}
+                  theme="outlined"
+                />
+                {/* <CustomerServiceOutlined /> */}
+              </Col>
+              <Col
+                span={18}
+                style={{
+                  textAlign: 'center',
+                }}
+              >
+                <div
+                  style={{
+                    marginLeft: 20,
+                    color: 'white',
+                    fontSize: 18,
+                  }}
+                >
+                  Jumlah Sewa Item
+                </div>
+                <div
+                  style={{
+                    marginLeft: 20,
+                    color: 'white',
+                    fontSize: 24,
+                  }}
+                >
+                  {sewaitem.length}
+                </div>
+              </Col>
+            </div>
+          </Col>
+        </Row>
+        <Row type="flex" style={{ marginTop: 20 }}>
+          <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+            <div
+              style={{
+                height: 120,
+                borderRadius: 10,
+                borderColor: 'grey',
+                margin: '5px 15px',
                 background: '#0F9D58',
                 borderWidth: 1,
                 padding: 20,
@@ -211,6 +295,107 @@ export class Dashboard extends Component {
               </Col>
             </div>
           </Col>
+
+          <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+            <div
+              style={{
+                height: 120,
+                borderRadius: 10,
+                borderColor: 'grey',
+                margin: '5px 15px',
+                background: '#2808cb',
+                borderWidth: 1,
+                padding: 20,
+              }}
+            >
+              <Col span={6}>
+                <Icon
+                  type="profile"
+                  style={{
+                    fontSize: '60px',
+                    color: 'white',
+                  }}
+                  theme="outlined"
+                />
+                {/* <CustomerServiceOutlined /> */}
+              </Col>
+              <Col
+                span={18}
+                style={{
+                  textAlign: 'center',
+                }}
+              >
+                <div
+                  style={{
+                    marginLeft: 20,
+                    color: 'white',
+                    fontSize: 18,
+                  }}
+                >
+                  Jumlah Subkategori
+                </div>
+                <div
+                  style={{
+                    marginLeft: 20,
+                    color: 'white',
+                    fontSize: 24,
+                  }}
+                >
+                  {subkategori.length}
+                </div>
+              </Col>
+            </div>
+          </Col>
+          {/* <Col xs={24} sm={12} md={8} lg={8} xl={8}>
+            <div
+              style={{
+                height: 120,
+                borderRadius: 10,
+                borderColor: 'grey',
+                margin: '5px 15px',
+                background: '#e531da',
+                borderWidth: 1,
+                padding: 20,
+              }}
+            >
+              <Col span={6}>
+                <Icon
+                  type="gold"
+                  style={{
+                    fontSize: '60px',
+                    color: 'white',
+                  }}
+                  theme="outlined"
+                /> */}
+          {/* <CustomerServiceOutlined /> */}
+          {/* </Col>
+              <Col
+                span={18}
+                style={{
+                  textAlign: 'center',
+                }}
+              >
+                <div
+                  style={{
+                    marginLeft: 20,
+                    color: 'white',
+                    fontSize: 18,
+                  }}
+                >
+                  Jumlah Kategori
+                </div>
+                <div
+                  style={{
+                    marginLeft: 20,
+                    color: 'white',
+                    fontSize: 24,
+                  }}
+                >
+                  {kategori.length}
+                </div>
+              </Col>
+            </div>
+          </Col> */}
         </Row>
       </div>
     );
